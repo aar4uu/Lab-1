3106f103d13d75ab4e1e33f04b772f218a574664
aar4uu <aarjupawara1125@gmail.com>
2024-09-04 22:48:05 +0530

    Add files via upload

---
+++ L1-3.C
@@ -0,0 +1,33 @@
+// Program to calculate gross salary
+#include<stdio.h>
+int main()
+{
+	float basic, gross,da,hra;
+	printf("enter basic salary of an employee:");
+	scanf("%f",&basic);
+	
+	if(basic<=10000)
+	{
+		da=basic*0.8;
+		hra=basic*0.2;
+	}
+	else if(basic<=20000)
+	{
+	       da=basic*0.9;
+		hra=basic*0.25;	
+	}
+	
+	else
+	{
+		da=basic*0.95;
+		hra=basic*0.3;
+		
+	}
+	
+	gross=basic+hra+da;
+	
+	printf("gross salary of employee=%.2f", gross);
+	
+	return 0;
+}
+
